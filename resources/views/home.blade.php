68fd91702cce707af656dce82707a6328e3a6f7f
cpasadilla <122351496+cpasadilla@users.noreply.github.com>
2025-02-05 15:37:19 +0800

    pagination

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,7 +81,7 @@ window.location = "{{route('p.view')}}";
                     <div class="icon">
                       <i class="ion ion-pie-graph"></i>
                     </div>
-                    <a href={{route('customer')}} class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{route('customer')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                   </div>
                 </div>
                 <!-- ./col -->
